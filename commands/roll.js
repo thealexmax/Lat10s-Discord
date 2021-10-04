e1c8e5f0383a686f5488c61fdd78e5195f2a97b3
Alex. W <29147967+thealexmax@users.noreply.github.com>
2021-10-04 18:49:27 +0200

    Roll command 2.0, I'm bored edition

--- commands/roll.js
+++ commands/roll.js
@@ -5,8 +5,62 @@ module.exports = {
 		if(!args[0]) {
 			return message.channel.send(`You rolled a: ${Math.floor(Math.random()*6+1)}`);
 		}
-		let range = args[0].split("d");
-		//Math.random() * (max - min) + min
-		return message.channel.send(`You rolled a: ${Math.floor(Math.random()*(Math.floor(range[1])-Math.ceil(range[0])+1)+ Math.ceil(range[0]))}`);
+		console.log(args);
+		let diceNumber = args[0].split("d");
+		const diceList = [];
+		let result = Math.floor(Math.random()*diceNumber[1]+1);
+		diceList.push(result);
+		for(i = 1; i < diceNumber[0]; i++) {
+			let dice = Math.floor(Math.random()*diceNumber[1]+1);
+			result += dice;
+			diceList.push(dice);
+		}
+		if(!args[1]) {
+			return message.channel.send(`Rolled: [${diceList}], Result: ${result}`);
+		}
+		for(i = 1; i < args.length; i++) {
+			//Thanks stackoverflow, very cool!
+			let num = args[i].match(/\d+/g);
+			if(!num) {
+				num = diceNumber[1];
+			}
+			let letr =  args[i].match(/[a-zA-Z]+/g);
+			let exploded = [];
+			switch (letr[0]) {
+				case "e":
+					exploded = [];
+					diceList.forEach(element => {
+						if(element == num) {
+							let dice = Math.floor(Math.random()*diceNumber[1]+1);
+							result += dice;
+							exploded.push(dice);
+						}
+					});
+					console.log(diceList);
+					console.log(exploded);
+					return message.channel.send(`Rolled: [${diceList}], Exploded: [${exploded}], Result: ${result}`);
+				case "ie":
+					exploded = [];
+					diceList.forEach(element => {
+						if(element == num) {
+							let dice = Math.floor(Math.random()*diceNumber[1]+1);
+							result += dice;
+							exploded.push(dice);
+						}
+					});
+					exploded.forEach(element => {
+						if(element == num) {
+							let dice = Math.floor(Math.random()*diceNumber[1]+1);
+							result += dice;
+							exploded.push(dice);
+						} 
+					});
+					console.log(diceList);
+					console.log(exploded);
+					return message.channel.send(`Rolled: [${diceList}], Exploded: [${exploded}], Result: ${result}`);
+				default:
+					return message.channel.send("Unknown command");
+			}
+		}
 	},
 };

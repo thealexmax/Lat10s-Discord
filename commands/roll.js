8bde351a443b7bf74d52026a2a4add85760faafc
Alex. W <29147967+thealexmax@users.noreply.github.com>
2021-10-04 16:14:22 +0200

    Roll command now has extra functionality
    Use [prefix]roll ndm to get a number between n and m

--- commands/roll.js
+++ commands/roll.js
@@ -1,7 +1,12 @@
 module.exports = {
 	name: 'roll',
 	description: 'Rolls a dice',
-	execute(message) {
-        message.channel.send(`You rolled a: ${Math.floor(Math.random()*6+1)}`);
+	execute(message, args) {
+		if(!args[0]) {
+			return message.channel.send(`You rolled a: ${Math.floor(Math.random()*6+1)}`);
+		}
+		let range = args[0].split("d");
+		//Math.random() * (max - min) + min
+		return message.channel.send(`You rolled a: ${Math.floor(Math.random()*(Math.floor(range[1])-Math.ceil(range[0])+1)+ Math.ceil(range[0]))}`);
 	},
 };
